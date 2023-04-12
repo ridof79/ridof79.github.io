f27ca9457194b2613c57bceeee10dda917d817ed
Muhammad Ridho Fiqri <ridof79@hotmail.com>
2023-04-13 04:08:07 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "index.html",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Mabry pro'],
+    },
+    container : {
+      center: true,
+      padding: '16px'
+    },
+    extend: {
+      colors: {
+        primary: '#3b82f6',
+        secondary: '#64748b',
+        dark: '#172554'
+      },
+      screens: {
+        '2xl':'1320px'
+      },
+    },
+  },
+  plugins: [],
+}
